17a2521f0f733ef9d6f42186799b858fc1fbb5e4
junghooo <ljghoo32@gmail.com>
2024-01-11 14:28:48 +0000

    fio 3.5

---
+++ app/external/fio/fio-fio-3.5/debug.c
@@ -0,0 +1,17 @@
+#include <stdarg.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "debug.h"
+
+#ifdef FIO_INC_DEBUG
+void __dprint(int type, const char *str, ...)
+{
+	va_list args;
+
+	assert(type < FD_DEBUG_MAX);
+
+	va_start(args, str);
+	log_prevalist(type, str, args);
+	va_end(args);
+}
+#endif
---
+++ driver/core/kv_version.c
@@ -0,0 +1,9 @@
+#include "kv_version.h"
+
+const char* buildtime="2024_0111_1342";
+const char* hash="49e90488b3f5d866fe09fb4de92a9df9a7929015";
+const char* os="GNU/Linux";
+const char* processor="x86_64";
+const char* kernel="5.4.0-64-generic";
+const char* dpdk_version="dpdk-18.05";
+const char* spdk_version="spdk-18.04.1";
